bbad7e6a09a0f1a2693398c023f5a3835cc81c9d
Avinash Joshi <avinash.joshi@dominodatalab.com>
2020-05-26 11:18:29 -0700

    bump up tro .11
    Pushed from Domino: https://workshop.cs.domino.tech/u/avinash-domino/model-export/runs/5ecd56a34f6ae700080db882

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_number_model",
-    version="0.0.10",
+    version="0.0.11",
     author="Akshay Ambekar",
     author_email="akshay.ambekar@dominodatalab.com",
     description="A random number model",

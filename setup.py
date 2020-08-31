3718a468044589a13293918e80a725e9b4f683f4
Avinash Joshi <avinash.joshi@dominodatalab.com>
2020-08-31 06:30:31 -0700

    Bumped the version.
    Pushed from Domino: https://demo.dominodatalab.com/u/amj2403/model-ops-demo/runs/5f4cf4abad3ea44259911844

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_number_model",
-    version="0.0.21",
+    version="0.0.22",
     author="Avinash Joshi",
     author_email="akshay.ambekar@dominodatalab.com",
     description="A random number model",

4708db364d73ebc96a0b2d67b6be00d1cc223b6f
Avinash Joshi <avinash.joshi@dominodatalab.com>
2020-10-12 08:17:25 -0700

    bump up the version number after fixing a typo
    Pushed from Domino: https://demo.dominodatalab.com/u/amj2403/model-ops-demo/runs/5f8462bc66c3b1007ffc75f5

--- BuildAndExportModelToECR.py
+++ BuildAndExportModelToECR.py
@@ -9,7 +9,7 @@ user_api_key = os.environ['DOMINO_USER_API_KEY']
 with open('setup.py') as f:
     c = f.read()
     
-modelName = "random-model-0.0.25"
+modelName = "random-model-0.0.26"
 
 def getOwnerId():
 	logging.info('Getting ownerId')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_number_model",
-    version="0.0.25",
+    version="0.0.26",
     author="Avinash Joshi",
     author_email="akshay.ambekar@dominodatalab.com",
     description="A random number model",

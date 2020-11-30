cfc6e4f64086a133ef37ebc9256278eb5554668d
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2020-11-29 22:33:44 -0800

    Pushed from Domino: https://demo.dominodatalab.com/u/akshay_ambekar/model-ops-demo/runs/5fc48c830ac6130d45a92de2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_number_model",
-    version="0.0.30",
+    version="0.0.31",
     author="Akshay Ambekar",
     author_email="akshay.ambekar@dominodatalab.com",
     description="A random number model",

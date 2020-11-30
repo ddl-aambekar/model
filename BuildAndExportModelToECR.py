40429d063d19b906515cb1a15e293b265491990d
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2020-11-29 22:40:15 -0800

    Bumping up the model version
    Pushed from Domino: https://demo.dominodatalab.com/u/akshay_ambekar/model-ops-demo/runs/5fc48c830ac6130d45a92de2

--- BuildAndExportModelToECR.py
+++ BuildAndExportModelToECR.py
@@ -9,7 +9,7 @@ user_api_key = os.environ['DOMINO_USER_API_KEY']
 with open('setup.py') as f:
     c = f.read()
     
-modelName = "random-model-0.0.31"
+modelName = "random-model-0.0.32"
 
 def getOwnerId():
 	logging.info('Getting ownerId')

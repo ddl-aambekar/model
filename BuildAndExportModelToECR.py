c405e9a933f2425d5c96f4b95cce0501cb5bb261
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2020-11-30 08:57:41 -0800

    Pushed from Domino: https://demo.dominodatalab.com/u/akshay_ambekar/model-ops-demo/runs/5fc5242f0de5e1255a27b6a6

--- BuildAndExportModelToECR.py
+++ BuildAndExportModelToECR.py
@@ -9,7 +9,7 @@ user_api_key = os.environ['DOMINO_USER_API_KEY']
 with open('setup.py') as f:
     c = f.read()
     
-modelName = "random-model-0.0.32"
+modelName = "random-model-0.0.33"
 
 def getOwnerId():
 	logging.info('Getting ownerId')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="random_number_model",
-    version="0.0.32",
+    version="0.0.33",
     author="Akshay Ambekar",
     author_email="akshay.ambekar@dominodatalab.com",
     description="A random number model",

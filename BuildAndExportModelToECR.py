48191ae08c973165a0adeb05f886ff677b62a51f
Akshay Ambekar <akshay.ambekar@dominodatalab.com>
2021-02-17 17:19:35 +0000

    bump

--- BuildAndExportModelToECR.py
+++ BuildAndExportModelToECR.py
@@ -9,7 +9,7 @@ user_api_key = os.environ['DOMINO_USER_API_KEY']
 with open('setup.py') as f:
     c = f.read()
     
-modelName = "random-model-0.0.36"
+modelName = "random-model-0.0.37"
 
 def getOwnerId():
 	logging.info('Getting ownerId')
